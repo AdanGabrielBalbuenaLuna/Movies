7205c73138538b7d424af96ea49ce318474466dd
Gabriel <agbalbuenaluna@gmail.com>
2025-03-29 12:40:21 -0600

    Very old fashion way to call a listener action

--- app/src/main/java/com/example/moviesdevexpert/MainActivity.kt
+++ app/src/main/java/com/example/moviesdevexpert/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.moviesdevexpert
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,7 +22,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.message.text = "Hello Gabriel"
+        binding.button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val messageInputText = binding.inputText.text.toString()
+                Toast.makeText(binding.root.context, messageInputText, Toast.LENGTH_LONG).show()
+            }
+        })
         Log.d("MainActivity", "onCreate")
     }
 
