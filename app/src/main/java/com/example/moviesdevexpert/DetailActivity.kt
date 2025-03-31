4d2b33a1cc51249ef416b3f159222bed00cc290a
Gabriel Balbuena <79127704+AdanGabrielBalbuenaLuna@users.noreply.github.com>
2025-03-31 13:09:00 -0600

    [UI] Send and receive a complete OBJECT from an activity to another - PARCELABLE UPDATE}

--- app/src/main/java/com/example/moviesdevexpert/DetailActivity.kt
+++ app/src/main/java/com/example/moviesdevexpert/DetailActivity.kt
@@ -1,6 +1,8 @@
 package com.example.moviesdevexpert
 
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi // Call requires API level 33
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.moviesdevexpert.databinding.ActivityDetailBinding
@@ -11,12 +13,13 @@ class DetailActivity : AppCompatActivity() {
     companion object {
         val EXTRA_MOVIE = "DetailActivity:title"
     }
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU) // Call requires API level 33 (current min is 24): android. content. Intent#getParcelableExtra Toggle info (Ctrl+F1)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val movie = intent.getParcelableExtra(EXTRA_MOVIE, Movie::class.java)
+        val movie = intent.getParcelableExtra(EXTRA_MOVIE, Movie::class.java) // Call requires API level 33 (current min is 24)
 
         if (movie != null) {
             binding.textView.text = movie.title
